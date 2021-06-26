76934ed259203b610ea4fcf8ddf324434ae4c932
Dennis Mwaniki <damos5776@gmail.com>
2021-06-26 21:26:29 +0300

    toppings price

--- js/script.js
+++ js/script.js
@@ -18,9 +18,46 @@ var telinput=$('#tel').val();
 
 var newdets=new details(firstnameinput,lastnameinput,emailinput,telinput);
 
-alert(newdets.firstname)
 
 
+
+//gather user pizza details
+var pizzaprice=0;
+var toppingsprice=0;
+var crustprice=0;
+
+var size=$('#size').val();
+var flavour=$('#flavour').val();
+var crust=$('#crust').val();
+var toppings=$('#toppings').val();
+
+if(size==='Small'){
+    pizzaprice +=300;
+}
+else if
+(size==='Medium'){
+    pizzaprice +=600;
+}
+else if
+(size==='Large'){
+    pizzaprice+=1200;
+}
+
+//toppings price depending on size
+
+if(size==='Small'){
+toppingsprice +=70;
+}
+else if
+(size==='Medium'){
+    toppingsprice +=100;
+}
+else if
+(size==='Large'){
+    toppingsprice+=120;
+}
+
+alert(toppingsprice)
 
     })
 })
